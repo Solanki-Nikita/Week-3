6679cbadbb055d9ca25aa266e0f15c2ceb773ebf
Solanki_Nikita <solankinikita020@gmail.com>
2026-02-24 16:52:56 +0530

    initial Update

--- Day_2/productCard/src/App.jsx
+++ Day_2/productCard/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import ProductCard from "./ProductCard";
+import "./assets/Headphones.jpg";
 
 export default function App() {
+  const handleAddToCart = (product) => {
+    console.log("Added:", product);
+  };
+
   return (
     <div>
-      
+      <ProductCard
+        title="Wireless Headphones"
+        price={2999}
+        image="productCard/src/assets/headphones.jpg"
+        description="High quality noise cancelling headphones."
+        onAddToCart={() => handleAddToCart("Wireless Headphones")}
+      />
     </div>
-  )
+  );
 }
---
+++ Day_2/productCard/src/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+const Card = () => {
+  return (
+    <div>
+        <ProductCard />
+    </div>
+
+  )
+}
+
+export default Card
