503a91fa0f09ba5f2168780facd311dfe9751f41
Solanki_Nikita <solankinikita020@gmail.com>
2026-02-24 15:01:28 +0530

    initial commit

---
+++ Day_1/counterApp/src/reduce.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
+} 
+
+
+export default function App() {
+
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+
+  return (
+    <div>
+      <h1> Counter App</h1>
+      <p>Count: {state.count}</p> 
+      <button onClick={()=>dispatch({type:"increment"})}>+</button>
+      <button onClick={()=>dispatch({type:"decrement"})}>-</button>
+      
+    </div>
+  )
+}
